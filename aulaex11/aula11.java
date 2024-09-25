6a4900fd2b2de0062bb7b72d5d27e21b7ae7cc90
jwaynesz <141192949+jwaynesz@users.noreply.github.com>
2024-09-25 15:01:57 -0300

    Add files via upload

---
+++ aulaex11/aula11.java
@@ -0,0 +1,14 @@
+package aulaex11;
+
+/**
+ *
+ * @author John Wayne
+ */
+public class aula11 {
+   public static void main(String[] args){
+       int contador =0;
+       while (contador <5){
+           contador++;   
+       }
+   } 
+}
