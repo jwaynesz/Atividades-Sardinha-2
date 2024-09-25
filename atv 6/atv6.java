817e686618e3ad6a338e79af55981cad0d99c3c5
John Wayne <141192949+jwaynesz@users.noreply.github.com>
2024-09-25 15:13:57 -0300

    Add files via upload

---
+++ atv 6/atv6.java
@@ -0,0 +1,20 @@
+
+package aula2;
+
+import java.util.*;
+
+public class atv6 {
+    public static void main(String[] args) {
+    Scanner x = new Scanner(System.in);
+    
+    double raio, area;
+    
+        System.out.println("Digite o Raio do circulo");
+        raio = x.nextDouble();
+        
+        area = math.pi
+
+    
+    
+    }
+}
