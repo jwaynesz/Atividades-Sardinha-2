1b8a6c1b868ec40e7119349c851f7d7aa2300d36
John Wayne <141192949+jwaynesz@users.noreply.github.com>
2024-09-25 15:13:17 -0300

    Add files via upload

---
+++ atv 5/atv5.java
@@ -0,0 +1,20 @@
+
+package aula2;
+import java.util.*;
+public class atv5 {
+    public static void main(String[] args) {
+    Scanner x = new Scanner(System.in);
+    
+    int raio, area;
+    
+    double pi = 3,14;
+        System.out.println("Digite o Raio da cicunferencia");
+        raio = x.nextInt();
+        
+        area = (pi*raio^2);
+        
+        System.out.println("");
+    
+    
+    }
+}
