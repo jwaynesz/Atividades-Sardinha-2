dab9e92f3319aadaa9eea9a11637e2efd36e1cc0
John Wayne <141192949+jwaynesz@users.noreply.github.com>
2024-09-25 15:12:04 -0300

    Add files via upload

---
+++ atv 1/atv1.java
@@ -0,0 +1,14 @@
+
+package aula2;
+
+public class atv1 {
+    
+        public static void main(String[] args) {
+        
+        System.out.println("Meu nome é John Wayne");
+        System.out.println("Eu tenho 20 anos");
+        
+        
+    
+        }
+}
