d355ddf0d5aa952f1083c8d9cd1626d27d7e7912
jwaynesz <141192949+jwaynesz@users.noreply.github.com>
2024-09-25 15:03:13 -0300

    Add files via upload

---
+++ aulaex18/aula18.java
@@ -0,0 +1,17 @@
+package aulaex18;
+
+/**
+ *
+ * @author John Wayne
+ */
+public class aula18 {
+   public static void main(String[] args){
+   int incr = 1;
+   int decr = 15;
+   for (int i = 0; i <16; i++){
+       System.out.println(incr+ "/"  +decr);
+    incr++;
+    decr--;
+   }
+   }
+   } 
