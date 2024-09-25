d10208edf15ad46e939b44eb45eff9e99e3da0df
jwaynesz <141192949+jwaynesz@users.noreply.github.com>
2024-09-25 15:02:56 -0300

    Add files via upload

---
+++ aulaex16/usuario.java
@@ -0,0 +1,27 @@
+package aulaex16;
+
+/**
+ *
+ * @author John Wayne
+ */
+public class usuario {
+    private String nome = "renan";
+    private String senha = "1234";
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+ 
+}
