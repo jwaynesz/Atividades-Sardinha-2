41503c6ce17842f884092979c7f9db06e35af6e8
John Wayne <141192949+jwaynesz@users.noreply.github.com>
2024-09-25 15:12:25 -0300

    Add files via upload

---
+++ atv 2/Aula2.java
@@ -0,0 +1,14 @@
+
+package aula2;
+public class Aula2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Digite uma mensagem");
+        System.out.println("Digite a sua idade");
+        
+        
+        
+    }
+    
+}
---
+++ atv 2/atv2.java
@@ -0,0 +1,25 @@
+
+package aula2;
+import java.util.Scanner;
+public class atv2 {
+        public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int n1, n2, soma;
+                       
+        System.out.println("Digite o primeiro numero");
+        n1 = scan.nextInt();
+        
+        
+        System.out.println("Digite o segundo numero");
+        n2 = scan.nextInt();
+        
+        soma = n1+n2;
+        
+            System.out.println("A soma dos dois numeros e: " + soma);
+        
+            
+        }
+    
+}
