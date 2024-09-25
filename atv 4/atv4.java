78a8458df87648849324a9d71d9a52ed7146a58e
John Wayne <141192949+jwaynesz@users.noreply.github.com>
2024-09-25 15:12:45 -0300

    Add files via upload

---
+++ atv 4/atv4.java
@@ -0,0 +1,25 @@
+
+package aula2;
+public static void main(String[] args) {
+    Scanner x = new Scanner(System.in);
+    
+    int nota1, nota2, media;
+import java.util.*;
+public class atv4 {
+    
+    
+        System.out.println("Digite a primeira nota");
+        nota1 = x.nextInt();
+        
+        System.out.println("Digite a segunda nota");
+        nota2 = x.nextInt();
+        
+        media = (nota1+nota2)/2;
+        
+        System.out.println("A media das notas e: " + media);
+        
+        x.close();
+            
+}
+
+}
