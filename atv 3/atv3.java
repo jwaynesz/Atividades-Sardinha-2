45c403f7ee94c3d28a35a4d85befb574ed9643e6
John Wayne <141192949+jwaynesz@users.noreply.github.com>
2024-09-25 15:12:33 -0300

    Add files via upload

---
+++ atv 3/atv3.java
@@ -0,0 +1,20 @@
+
+package aula2;
+import java.util.Scanner;
+public class atv3 {
+    
+        public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int numero;
+        
+            System.out.println("Digite um numero");
+            numero = scan.nextInt();
+            
+            
+       
+    
+}
+
+}
